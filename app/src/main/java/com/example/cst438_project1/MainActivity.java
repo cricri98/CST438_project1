e728fcaf820f0cde725db2cb20f387944d132455
rainbowsprinkles <pikchu1998@gmail.com>
2020-01-28 17:46:06 -0800

    Added Course class

--- app/src/main/java/com/example/cst438_project1/MainActivity.java
+++ app/src/main/java/com/example/cst438_project1/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.cst438_project1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
