0db9f7bdfcf6e604aefa1e427775b00f32ab5df1
rainbowsprinkles <pikchu1998@gmail.com>
2020-01-30 17:05:54 -0800

    Added course

--- app/src/main/java/com/example/cst438_project1/CourseDAO/Course.java
+++ app/src/main/java/com/example/cst438_project1/CourseDAO/Course.java
@@ -6,9 +6,73 @@ import androidx.room.PrimaryKey;
 
 @Entity
 public class Course{
-    @PrimaryKey
-    public int courseId;
+    @PrimaryKey(autoGenerate = true)
+    private int mCourseId;
 
     @ColumnInfo(name = "course-name")
-    public String courseName;
+    private String mCourseName;
+
+    private int mInstructorId;
+
+    //formatted like mmddyyyy
+    private String mStartDate;
+    private String mEndDate;
+
+    private String mDescription;
+
+    public Course(String courseName, int instructorId, String startDate, String endDate, String description) {
+        mCourseName = courseName;
+        mInstructorId = instructorId;
+        mStartDate = startDate;
+        mEndDate = endDate;
+        mDescription = description;
+    }
+
+    public int getCourseId() {
+        return mCourseId;
+    }
+
+    public void setCourseId(int courseId) {
+        mCourseId = courseId;
+    }
+
+    public String getCourseName() {
+        return mCourseName;
+    }
+
+    public void setCourseName(String courseName) {
+        mCourseName = courseName;
+    }
+
+    public int getInstructorId() {
+        return mInstructorId;
+    }
+
+    public void setInstructorId(int instructorId) {
+        mInstructorId = instructorId;
+    }
+
+    public String getStartDate() {
+        return mStartDate;
+    }
+
+    public void setStartDate(String startDate) {
+        mStartDate = startDate;
+    }
+
+    public String getEndDate() {
+        return mEndDate;
+    }
+
+    public void setEndDate(String endDate) {
+        mEndDate = endDate;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
 }
