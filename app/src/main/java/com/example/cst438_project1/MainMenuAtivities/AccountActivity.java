cb4cdaf2448a1923a86f01dbdb254d39d9f15aaa
JoshuaClick <59895515+joshuaclick@users.noreply.github.com>
2020-02-20 15:29:06 -0800

    Added EditAccountActivity.
    TODO: Figure out how to update AccountActivity when coming back.

--- app/src/main/java/com/example/cst438_project1/MainMenuAtivities/AccountActivity.java
+++ app/src/main/java/com/example/cst438_project1/MainMenuAtivities/AccountActivity.java
@@ -1,6 +1,9 @@
 package com.example.cst438_project1.MainMenuAtivities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -8,17 +11,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
 import com.example.cst438_project1.DB.StudentAppDatabase;
+import com.example.cst438_project1.MainMenuAtivities.ViewAssignment.ViewAssignmentActivity;
 import com.example.cst438_project1.Objects.User;
 import com.example.cst438_project1.R;
 
 public class AccountActivity extends AppCompatActivity {
 
+    int userId;
     User u;
     StudentAppDatabase db;
 
     TextView usernameView;
     TextView nameView;
     TextView userIdView;
+
+    Button editAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +34,7 @@ public class AccountActivity extends AppCompatActivity {
 
         db = Room.databaseBuilder(getApplicationContext(), StudentAppDatabase.class, "db").allowMainThreadQueries().build();
 
-        int userId = getIntent().getIntExtra("userId", -1);
+        userId = getIntent().getIntExtra("userId", -1);
 
         u = db.getUserDao().getUserById(userId);
 
@@ -35,6 +42,13 @@ public class AccountActivity extends AppCompatActivity {
         nameView = findViewById(R.id.nameView);
         userIdView = findViewById(R.id.userIdView);
 
+        editAccount = findViewById(R.id.editAccount);
+        editAccount.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                editAccountActivity();
+            }
+        });
         updateViews();
     }
 
@@ -54,5 +68,11 @@ public class AccountActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         finish();
+    }
+
+    public void editAccountActivity(){
+        Intent i = new Intent(this, EditAccountActivity.class);
+        i.putExtra("userId", userId);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/cst438_project1/MainMenuAtivities/EditAccountActivity.java
@@ -0,0 +1,125 @@
+package com.example.cst438_project1.MainMenuAtivities;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import com.example.cst438_project1.DB.StudentAppDatabase;
+import com.example.cst438_project1.Objects.User;
+import com.example.cst438_project1.R;
+
+public class EditAccountActivity extends AppCompatActivity {
+
+    Button saveAccountButton;
+
+    TextView tempUsers;
+
+    StudentAppDatabase db;
+
+    int userId;
+    User currentUser;
+
+    EditText username;
+    EditText name;
+    EditText password;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_account);
+        db = Room.databaseBuilder(getApplicationContext(), StudentAppDatabase.class, "db").allowMainThreadQueries().build();
+
+        saveAccountButton = findViewById(R.id.save_account);
+        //tempUsers = findViewById(R.id.tempUserView);
+
+        userId = getIntent().getIntExtra("userId", -1);
+        currentUser = db.getUserDao().getUserById(userId);
+
+        username = findViewById(R.id.edit_usernameText);
+        name = findViewById(R.id.edit_nameText);
+        password = findViewById(R.id.edit_passwordText);
+
+
+        //Enter account info and validate credentials
+        saveAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                enterAccountInfo();
+            }
+        });
+
+        populate();
+        //updateTempText();
+/*
+        tempUsers.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                for(User u : db.getUserDao().getAll()){
+                    db.getUserDao().deleteUser(u);
+                }
+                updateTempText();
+                return false;
+            }
+        });
+*/
+    }
+
+    private void updateTempText(){
+        String temp = "";
+        User[] users = db.getUserDao().getAll();
+
+        for(User us : users){
+            temp = temp + us + '\n';
+        }
+        tempUsers.setText(temp);
+    }
+
+
+    //checks to see if username is duplicate or invalid
+    //has no constraints for username format, yet
+    void enterAccountInfo(){
+        //retrieve username and password
+        String savedUsername = username.getText().toString().trim();
+        String savedName = name.getText().toString().trim();
+        String savedPassword = password.getText().toString().trim();
+
+        if(savedUsername.equals("") || savedName.equals("") || savedPassword.equals("")){
+            Toast.makeText(getApplicationContext(), R.string.aboveIsEmpty, Toast.LENGTH_LONG).show();
+        }else {
+
+            try {
+                currentUser.setName(savedName);
+                currentUser.setUsername(savedUsername);
+                currentUser.setPassword(savedPassword);
+
+                db.getUserDao().updateUser(currentUser);
+
+                finish();
+            } catch (Resources.NotFoundException e) {
+                Toast.makeText(getApplicationContext(), R.string.usernameError, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+    public void populate(){
+        if (currentUser != null) {
+            username.setText(currentUser.getUsername());
+            name.setText(currentUser.getName());
+            password.setText(currentUser.getPassword());
+        }
+    }
+}
